d9c6a42e38e26df80c849f76479f126db2dd1c5f
ex7689 <ex7689@wayne.edu>
2014-08-20 15:30:55 -0400

    first commit

---
+++ compare_foldChange.R
@@ -0,0 +1,106 @@
+#######
+## Script to compare FC of DE genes between shallow and deep
+## input
+##	plate: plate number, e.g., P5
+##	fdrThresh: FDR cutoff for significance, e.g., 0.2
+## G Moyerbrailean, Wayne State University
+#######
+
+library(ggplot2)
+require(parallel)
+
+cargs<-commandArgs(trail=TRUE);
+if(length(cargs)>=1)
+plate<-cargs[1];
+if(length(cargs)>=2)
+fdrThresh<-cargs[2]
+if(length(cargs)>=3)
+baseDir<-cargs[3]
+if(length(cargs)>=4)
+cores<-cargs[4]
+
+fdrThresh <- as.numeric(fdrThresh)
+cores <- as.numeric(cores)
+
+if(cores<1){cores <- 1}
+ParallelSapply <- function(...,mc.cores=cores){
+  simplify2array(mclapply(...,mc.cores=mc.cores))
+}
+
+## Get treatment IDs and treatment names
+txTable <- read.table('etc/txID_to_name.txt', as.is=T, header=T, sep='\t')
+rownames(txTable) <- txTable$Treatment_ID
+
+# Data Structure - 
+# plots/
+#	plate/
+#		Tx1.pdf
+#		Tx2.pdf
+system(paste0('mkdir -p plots/fold_change/', plate))
+
+## Get the fold change data for treatments sequenced twice
+deepFiles = list.files(paste0(baseDir, "out_data_D", plate, "/stats/"))
+ParallelSapply(deepFiles, function(dFile){
+
+	txID = gsub('.txt', '', gsub('.*_', '', dFile))
+	sFile=gsub('DP', 'P', dFile)
+	cmd = paste0("less ", baseDir, "out_data_", plate, "/stats/", sFile)
+	shal <- read.table(pipe(cmd), as.is=T, sep=' ', header=T)
+	cmd = paste0("less ", baseDir, "out_data_D", plate, "/stats/", dFile)
+	deep <- read.table(pipe(cmd), as.is=T, sep=' ', header=T)
+	names(shal) <- c("t.id", "qv", "pval", "logFC", "ensg", "g.id")
+	names(deep) <- names(shal)
+
+	## Get the transcripts in both data sets
+	both <- shal$t.id[shal$t.id %in% deep$t.id]
+	shal <- shal[shal$t.id %in% both,]
+	deep <- deep[deep$t.id %in% both,]
+
+	## sanity check - ensure sorted
+	stopifnot(shal$t.id == deep$t.id)
+
+	## Combine and format the data for easy plotting
+	dat <- data.frame(t.id=shal$t.id, ensg=shal$ensg, g.id=shal$g.id, 
+		sFC=shal$logFC, sQ=shal$qv, sP=shal$pval, dFC=deep$logFC, 
+		dQ=deep$qv, dP=deep$pval)
+	
+	## Check the correlation between shallow and deep data
+	corr <- cor.test(dat$sFC, dat$dFC, method="spearman")
+	# cat('#', 'cor:', corr$estimate, '\n')
+	# cat('#', 'pval:', corr$p.value, '\n')
+
+	## Assign color by significance threshold
+	dat$sig <- "All"
+	dat$sig[dat$sQ<fdrThresh] <- "Shallow"
+	dat$sig[dat$dQ<fdrThresh] <- "Deep"
+	dat$sig[dat$sQ<fdrThresh & dat$dQ<fdrThresh] <- "Both"
+
+	## Plot!
+	png(paste0('plots/fold_change/', plate, '_', txID, '.png'))
+	plot(dat$sFC, dat$dFC, pch=20, col="black", xlab="logFC - Shallow", ylab="logFC - Deep",
+		main=paste0("Plates ", plate, "/D", plate, ": ", txTable[txID,]$Short_Name, 
+		 	    " (", fdrThresh*100, "% FDR)"))
+	points(dat$sFC[dat$sig=='Deep'], dat$dFC[dat$sig=='Deep'], col='red', pch=20)
+	points(dat$sFC[dat$sig=='Shallow'], dat$dFC[dat$sig=='Shallow'], col='blue', pch=20)
+	points(dat$sFC[dat$sig=='Both'], dat$dFC[dat$sig=='Both'], col='purple', pch=20)
+	legend("topleft", 
+		legend=c(paste0("All (", dim(dat)[1],")"),
+				 paste0("Both (", dim(dat[dat$sig=="Both",])[1], ")"),
+				 paste0("Deep only (", dim(dat[dat$sig=="Deep",])[1], ")"),
+				 paste0("Shallow only (", dim(dat[dat$sig=="Shallow",])[1], ")")), 
+		col=c("black", "purple", "red", "blue"), pch=20, 
+		title=paste0("Spearman: ", round(corr$estimate,2), ", p=", corr$p.value))
+	abline(v=0, lty=3)
+	abline(h=0, lty=3)
+	dev.off()
+
+	# Try with just the significantly DE transcripts
+	dat2 <- dat[dat$sig != "All",]
+	p1 <- ggplot(dat2, aes(x=sFC, y=dFC)) 
+	p1 + 
+		geom_point(alpha=0.75, aes(color=sig)) +
+		scale_colour_manual(values = c("purple", "red", "blue")) +
+		xlab("logFC - Shallow") +
+		ylab("logFC - Deep")
+	ggsave(paste0('plots/fold_change/', plate, '_', txID, '_sig.png'))
+})
---
+++ gen_DEG_table.py
@@ -0,0 +1,34 @@
+import sys
+
+## TODO: use sys.argv to get base folder
+degBase="/wsu/home/groups/piquelab/charvey/GxE/differential_expression/DEseq2_results/"
+
+## TODO: use sys.argv to get plate list
+plateList = [1, 2, 4, 5, 7, 8, 11, 12]
+
+outTable = ''
+## Get DEG data from shallow sequencing
+for p in plateList:
+	fd = open(degBase + "out_data_P%s/data_DEG/P%s_DEG.txt" % (p, p))
+	dat = fd.read().split('\n')
+	if '' in dat:
+		dat.remove('')
+	for l in dat[1:]: # skip header
+		l = l + '\tP%s\tshallow\n' % p
+		outTable += l
+	fd.close()
+for p in plateList:
+	fd = open(degBase + "out_data_DP%s/data_DEG/DP%s_DEG.txt" % (p, p))
+	dat = fd.read().split('\n')
+	if '' in dat:
+		dat.remove('')
+	for l in dat[1:]: # skip header
+		l = l + '\tP%s\tdeep\n' % p
+		outTable += l
+	fd.close()
+
+## output DEG table
+fd = open('DEG_table.txt', 'w')
+fd.write('Treatment.ID\tTreatment\tFDR.01\tFDR.05\tFDR.1\tFDR.2\tpi0\tPlate\tDepth\n')
+fd.write(outTable)
+fd.close()
