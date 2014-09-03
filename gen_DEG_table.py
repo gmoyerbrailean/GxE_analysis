c210a6b38aa45af155bb669bb74d9deb1d60039b
ex7689 <ex7689@wayne.edu>
2014-09-03 12:41:14 -0400

    Added try/catch to handle non-existant files

--- gen_DEG_table.py
+++ gen_DEG_table.py
@@ -4,28 +4,34 @@ import sys
 degBase="/wsu/home/groups/piquelab/charvey/GxE/differential_expression/DEseq2_results/"
 
 ## TODO: use sys.argv to get plate list
-plateList = [1, 2, 4, 5, 7, 8, 11, 12]
+plateList = [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 outTable = ''
 ## Get DEG data from shallow sequencing
 for p in plateList:
-	fd = open(degBase + "out_data_P%s/data_DEG/P%s_DEG.txt" % (p, p))
-	dat = fd.read().split('\n')
-	if '' in dat:
-		dat.remove('')
-	for l in dat[1:]: # skip header
-		l = l + '\tP%s\tshallow\n' % p
-		outTable += l
-	fd.close()
+	try:
+		fd = open(degBase + "out_data_P%s/data_DEG/P%s_DEG.txt" % (p, p))
+		dat = fd.read().split('\n')
+		if '' in dat:
+			dat.remove('')
+		for l in dat[1:]: # skip header
+			l = l + '\tP%s\tshallow\n' % p
+			outTable += l
+		fd.close()
+	except:
+		continue
 for p in plateList:
-	fd = open(degBase + "out_data_DP%s/data_DEG/DP%s_DEG.txt" % (p, p))
-	dat = fd.read().split('\n')
-	if '' in dat:
-		dat.remove('')
-	for l in dat[1:]: # skip header
-		l = l + '\tP%s\tdeep\n' % p
-		outTable += l
-	fd.close()
+	try:
+		fd = open(degBase + "out_data_DP%s/data_DEG/DP%s_DEG.txt" % (p, p))
+		dat = fd.read().split('\n')
+		if '' in dat:
+			dat.remove('')
+		for l in dat[1:]: # skip header
+			l = l + '\tP%s\tdeep\n' % p
+			outTable += l
+		fd.close()
+	except:
+		continue
 
 ## output DEG table
 fd = open('DEG_table.txt', 'w')
