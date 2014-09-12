5f238baca903d296ff2948d133909dfb756803c6
ex7689 <ex7689@wayne.edu>
2014-09-12 13:32:22 -0400

    Fixed cv rownames, added header

--- load_cv_tables.R
+++ load_cv_tables.R
@@ -1,3 +1,8 @@
+## Script to source in covariate table for a given plate, and the master treatment table
+## Usage: source(/path/to/script/load_cv_table.R)
+## Usage note: the environment you are sourcing into must have a "plate" variable set, e.g., P1, P2, etc
+## G Moyerbrailean, Wayne State University 2014
+
 LPG <- Sys.getenv("LPG")
 
 ## Load treatment and covariate data
@@ -6,6 +11,7 @@ row.names(treatmentKey) <- treatmentKey$Treatment_ID
 cov.file <- paste(LPG, '/charvey/GxE/derived_data/covariates/GxE_', plate, '_covariates.txt', sep='')
 cv <- read.table(cov.file , as.is=T, sep="\t", header=T, comment="")
 cv <- cv[order(cv$Barcode.ID),]
+rownames(cv) <- cv$Barcode.ID
 
 cv$Treatment.ID <- factor(cv$Treatment.ID)
 TreatmentLevels <- levels(cv$Treatment.ID)
