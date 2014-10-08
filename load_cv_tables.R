21da3b64f1d58cd732b5848b57684947049ae40b
ex7689 <ex7689@wayne.edu>
2014-10-08 18:10:22 -0400

    'plate' -> 'platePrefix' to be consistent with other scripts

--- load_cv_tables.R
+++ load_cv_tables.R
@@ -1,6 +1,6 @@
 ## Script to source in covariate table for a given plate, and the master treatment table
 ## Usage: source(/path/to/script/load_cv_table.R)
-## Usage note: the environment you are sourcing into must have a "plate" variable set, e.g., P1, P2, etc
+## Usage note: the environment you are sourcing into must have a "platePrefix" variable set, e.g., P1, DP2, etc
 ## G Moyerbrailean, Wayne State University 2014
 
 LPG <- Sys.getenv("LPG")
@@ -8,7 +8,7 @@ LPG <- Sys.getenv("LPG")
 ## Load treatment and covariate data
 treatmentKey <- read.table(paste(LPG, '/charvey/GxE/derived_data/covariates/GxE_treatment_key.txt', sep=''), sep='\t', as.is=TRUE, header=TRUE)
 row.names(treatmentKey) <- treatmentKey$Treatment_ID
-cov.file <- paste(LPG, '/charvey/GxE/derived_data/covariates/GxE_', plate, '_covariates.txt', sep='')
+cov.file <- paste(LPG, '/charvey/GxE/derived_data/covariates/GxE_', platePrefix, '_covariates.txt', sep='')
 cv <- read.table(cov.file , as.is=T, sep="\t", header=T, comment="")
 cv <- cv[order(cv$Barcode.ID),]
 rownames(cv) <- cv$Barcode.ID
